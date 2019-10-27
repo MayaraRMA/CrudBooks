4d911de699b6f8d1d8520d0c472af483d7a0966e
Mayara Alencar <mayara.rma@gmail.com>
2019-10-27 17:56:25 -0300

    git init

---
+++ CrudBooks/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace CrudBooks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.titleTxtBox = new System.Windows.Forms.TextBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.editionTxtBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.classTxtBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.isbnTxtBox = new System.Windows.Forms.MaskedTextBox();
+            this.yearTxtMask = new System.Windows.Forms.MaskedTextBox();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.turnOffButton = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(2, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(307, 188);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Candara", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gerenciador de Livros";
+            // 
+            // titleTxtBox
+            // 
+            this.titleTxtBox.Location = new System.Drawing.Point(405, 55);
+            this.titleTxtBox.Name = "titleTxtBox";
+            this.titleTxtBox.Size = new System.Drawing.Size(508, 27);
+            this.titleTxtBox.TabIndex = 2;
+            // 
+            // addBtn
+            // 
+            this.addBtn.BackColor = System.Drawing.Color.SteelBlue;
+            this.addBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.addBtn.FlatAppearance.BorderSize = 0;
+            this.addBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addBtn.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addBtn.ForeColor = System.Drawing.Color.White;
+            this.addBtn.Location = new System.Drawing.Point(303, 207);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(116, 29);
+            this.addBtn.TabIndex = 3;
+            this.addBtn.Text = "SALVAR";
+            this.addBtn.UseVisualStyleBackColor = false;
+            this.addBtn.Click += new System.EventHandler(this.AddBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(347, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Título:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(342, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Edição:";
+            // 
+            // editionTxtBox
+            // 
+            this.editionTxtBox.Location = new System.Drawing.Point(405, 149);
+            this.editionTxtBox.Name = "editionTxtBox";
+            this.editionTxtBox.Size = new System.Drawing.Size(265, 27);
+            this.editionTxtBox.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(300, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 19);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Classificação:";
+            // 
+            // classTxtBox
+            // 
+            this.classTxtBox.Location = new System.Drawing.Point(405, 101);
+            this.classTxtBox.Name = "classTxtBox";
+            this.classTxtBox.Size = new System.Drawing.Size(265, 27);
+            this.classTxtBox.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(693, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 19);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Ano:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(689, 153);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 19);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "ISBN:";
+            // 
+            // isbnTxtBox
+            // 
+            this.isbnTxtBox.Location = new System.Drawing.Point(740, 149);
+            this.isbnTxtBox.Mask = "000 - 00 - 000 - 000 - 0";
+            this.isbnTxtBox.Name = "isbnTxtBox";
+            this.isbnTxtBox.Size = new System.Drawing.Size(173, 27);
+            this.isbnTxtBox.TabIndex = 13;
+            // 
+            // yearTxtMask
+            // 
+            this.yearTxtMask.Location = new System.Drawing.Point(740, 101);
+            this.yearTxtMask.Mask = "0000";
+            this.yearTxtMask.Name = "yearTxtMask";
+            this.yearTxtMask.Size = new System.Drawing.Size(173, 27);
+            this.yearTxtMask.TabIndex = 14;
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.BackColor = System.Drawing.Color.SteelBlue;
+            this.updateBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.updateBtn.FlatAppearance.BorderSize = 0;
+            this.updateBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.updateBtn.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateBtn.ForeColor = System.Drawing.Color.White;
+            this.updateBtn.Location = new System.Drawing.Point(467, 207);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(116, 29);
+            this.updateBtn.TabIndex = 15;
+            this.updateBtn.Text = "ATUALIZAR";
+            this.updateBtn.UseVisualStyleBackColor = false;
+            this.updateBtn.Click += new System.EventHandler(this.UpdateBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.BackColor = System.Drawing.Color.SteelBlue;
+            this.deleteBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.deleteBtn.FlatAppearance.BorderSize = 0;
+            this.deleteBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.deleteBtn.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteBtn.ForeColor = System.Drawing.Color.White;
+            this.deleteBtn.Location = new System.Drawing.Point(631, 207);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(116, 29);
+            this.deleteBtn.TabIndex = 16;
+            this.deleteBtn.Text = "EXCLUIR";
+            this.deleteBtn.UseVisualStyleBackColor = false;
+            this.deleteBtn.Click += new System.EventHandler(this.DeleteBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.BackColor = System.Drawing.Color.SteelBlue;
+            this.clearBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.clearBtn.FlatAppearance.BorderSize = 0;
+            this.clearBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clearBtn.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearBtn.ForeColor = System.Drawing.Color.White;
+            this.clearBtn.Location = new System.Drawing.Point(795, 207);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(116, 29);
+            this.clearBtn.TabIndex = 17;
+            this.clearBtn.Text = "LIMPAR";
+            this.clearBtn.UseVisualStyleBackColor = false;
+            this.clearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // turnOffButton
+            // 
+            this.turnOffButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("turnOffButton.BackgroundImage")));
+            this.turnOffButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.turnOffButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.turnOffButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.turnOffButton.FlatAppearance.BorderSize = 0;
+            this.turnOffButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.turnOffButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.turnOffButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.turnOffButton.Location = new System.Drawing.Point(891, 4);
+            this.turnOffButton.Name = "turnOffButton";
+            this.turnOffButton.Size = new System.Drawing.Size(36, 34);
+            this.turnOffButton.TabIndex = 18;
+            this.turnOffButton.UseVisualStyleBackColor = true;
+            this.turnOffButton.Click += new System.EventHandler(this.TurnOffButton_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(18, 269);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(893, 356);
+            this.listView1.TabIndex = 19;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ListView1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(939, 655);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.turnOffButton);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.yearTxtMask);
+            this.Controls.Add(this.isbnTxtBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.classTxtBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.editionTxtBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.titleTxtBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox titleTxtBox;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox editionTxtBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox classTxtBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox isbnTxtBox;
+        private System.Windows.Forms.MaskedTextBox yearTxtMask;
+        private System.Windows.Forms.Button updateBtn;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button turnOffButton;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ CrudBooks/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrudBooks
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //ListView Properties
+            listView1.View = View.Details;
+            listView1.FullRowSelect = true;
+
+            //Construct Columns
+            listView1.Columns.Add("Título", 400);
+            listView1.Columns.Add("Classificação", 150);
+            listView1.Columns.Add("Edição", 100);
+            listView1.Columns.Add("Ano", 100);
+            listView1.Columns.Add("ISBN", 200);    
+        }
+        //Add to Lv
+        private void add(String title, String classification, String edition, String year, String isbn)
+        {
+            String[] row = { title, classification, edition, year, isbn };
+
+            ListViewItem item = new ListViewItem(row);
+
+            listView1.Items.Add(item);
+        }
+
+        //update
+        private void update()
+        {
+            listView1.SelectedItems[0].SubItems[0].Text = titleTxtBox.Text;
+            listView1.SelectedItems[0].SubItems[1].Text = classTxtBox.Text;
+            listView1.SelectedItems[0].SubItems[2].Text = editionTxtBox.Text;
+            listView1.SelectedItems[0].SubItems[3].Text = yearTxtMask.Text;
+            listView1.SelectedItems[0].SubItems[4].Text = isbnTxtBox.Text;
+
+            //clear txt
+            titleTxtBox.Text = "";
+            classTxtBox.Text = "";
+            editionTxtBox.Text = "";
+            yearTxtMask.Text = "";
+            isbnTxtBox.Text = "";
+        }
+
+        private void delete()
+        {
+            if(MessageBox.Show("Tem certeza que deseja excluir esse livro?", "EXCLUIR", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning )==DialogResult.OK)
+            {
+                listView1.Items.RemoveAt(listView1.SelectedIndices[0]);
+
+                //clear txt
+                titleTxtBox.Text = "";
+                classTxtBox.Text = "";
+                editionTxtBox.Text = "";
+                yearTxtMask.Text = "";
+                isbnTxtBox.Text = "";
+            }
+        }
+
+        private void TurnOffButton_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            delete();
+        }
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+
+            //clear txt
+            titleTxtBox.Text = "";
+            classTxtBox.Text = "";
+            editionTxtBox.Text = "";
+            yearTxtMask.Text = "";
+            isbnTxtBox.Text = "";
+        }
+
+        private void UpdateBtn_Click(object sender, EventArgs e)
+        {
+            update();
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            add(titleTxtBox.Text, classTxtBox.Text, editionTxtBox.Text, yearTxtMask.Text, isbnTxtBox.Text);
+
+
+            //clear txt
+            titleTxtBox.Text = "";
+            classTxtBox.Text = "";
+            editionTxtBox.Text = "";
+            yearTxtMask.Text = "";
+            isbnTxtBox.Text = "";
+
+
+        }
+
+        private void ListView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            titleTxtBox.Text = listView1.SelectedItems[0].SubItems[0].Text;
+            classTxtBox.Text = listView1.SelectedItems[0].SubItems[1].Text;
+            editionTxtBox.Text = listView1.SelectedItems[0].SubItems[2].Text;
+            yearTxtMask.Text = listView1.SelectedItems[0].SubItems[3].Text;
+            isbnTxtBox.Text = listView1.SelectedItems[0].SubItems[4].Text;
+            
+
+        }
+    }
+}
